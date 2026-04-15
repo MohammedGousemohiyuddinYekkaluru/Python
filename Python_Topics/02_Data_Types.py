a3f16242b4a694f99dc2332374a654aadf494e65
mohammed gouse <gousemohammed163@gmail.com>
2026-04-15 16:34:50 +0530

    Add 02_Data_Types.py

---
+++ Python_Topics/02_Data_Types.py
@@ -0,0 +1,45 @@
+# Python supports 3 categories of data types:
+
+## Basic Types - integer, string, boolean, complex, float
+## Container Types - List, Tuples, Dictionary, Sets
+## User-Defined Types - Class
+
+# Integer
+
+print(4)
+
+# range of integer in python is more
+print(5000000000000000000000000000000000000)
+
+print(1e308) # it is the biggest integer in python
+
+print(1e309) # infinity
+
+# float
+
+print(4.5)
+print(1.7e+308) # it is the biggest float number in python
+
+# Boolean
+print(True)
+print(False)
+
+# Complex
+print(4+5j)
+
+# Strings
+print("Anantapur")
+print('Anantapur')
+print("""Anantapur""")
+
+# List
+print([1, 2, 3, 4, 5])
+
+# Tuples
+print((1, 2, 3, 4, 5))
+
+# Sets
+print({1, 2, 3, 4, 5})
+
+# Dictionary
+print({"Name": "Gouse", "Age": "23", "Gender": "Male"})
