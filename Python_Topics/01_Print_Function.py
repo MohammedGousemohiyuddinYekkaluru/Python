483bfe20dd5c4beea24cb1e45ec59503e3111275
mohammed gouse <gousemohammed163@gmail.com>
2026-04-15 16:21:58 +0530

    Add 01_Print_Function.py

---
+++ Python_Topics/01_Print_Function.py
@@ -0,0 +1,18 @@
+print("Hello World!")
+
+print(6)
+
+print(2.5)
+
+print(True)
+
+# we can print multiple things
+
+print("India", "Australia", "England", "South Africa")
+
+print("India", "Australia", "England", "South Africa", sep="/") # using sep => seperator
+
+print("Hello", end=" ") # changed end=" "(space) from end="\n"(new line)
+print("world")
+
+
