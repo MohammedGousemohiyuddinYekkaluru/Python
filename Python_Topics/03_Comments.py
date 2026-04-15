c46115c6e59dcc68c455f488057934d357a10ca3
mohammed gouse <gousemohammed163@gmail.com>
2026-04-15 16:40:35 +0530

    All about Comments in python

---
+++ Python_Topics/03_Comments.py
@@ -0,0 +1,9 @@
+# Comment => A piece of code which is not executable by the interpreter...
+
+# it is used by using "#"
+
+# it is used like a notes or to understand your own logic (use it like a documentation after some weeks/ months) you wrote in the code...
+
+## Basically it enhances code readability..
+
+# it is helpful in the longer run
