3815fdb264ecb69994aee663861b68b681e33b37
mohammed gouse <gousemohammed163@gmail.com>
2026-04-16 15:46:24 +0530

    All about taking input from the user

---
+++ Python_Topics/06_Taking_User_Input.py
@@ -0,0 +1,12 @@
+name = input("Enter your name : ")
+
+print(name)
+
+# Lets do a task => ask user for two numbers and perform addition to it and print the result
+
+first_num = input("Enter first number : ") #inputs gives strings # ex num1 = 3
+second_num = input("Enter second number : ") # num2 = 3
+
+result = first_num + second_num
+print(result) # 33
+
