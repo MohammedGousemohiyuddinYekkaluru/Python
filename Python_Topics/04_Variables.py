ed7b221480dfa3122ab0be8b21083d308848b6f4
mohammed gouse <gousemohammed163@gmail.com>
2026-04-16 15:27:02 +0530

    All about variables

---
+++ Python_Topics/04_Variables.py
@@ -0,0 +1,31 @@
+# Variables => Are like containers to store information for future use
+# python has no variable declaration
+
+name = "Gouse"
+print(name)
+
+age = 23
+print(age)
+
+# Python is "dynamic typing" language, means we dont need to specify the datatype for the variable...
+
+name = 4
+print(name)
+
+# Dynamic binding => a variable can store any data type and like the above example i redeclared the string to the integer...
+
+# Special syntax
+a=5; b=6; c=7
+print(a)
+print(b)
+print(c)
+
+a = b = c = 6
+print(a)
+print(b)
+print(c)
+
+a,b,c = 4,5,6
+print(a)
+print(b)
+print(c)
