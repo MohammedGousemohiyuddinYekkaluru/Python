32834b087907572900fff36fc7c63eea449cd768
mohammed gouse <gousemohammed163@gmail.com>
2026-04-16 15:34:51 +0530

    All about Keywords and Identifiers

---
+++ Python_Topics/05_Keywords_&_Identifiers.py
@@ -0,0 +1,17 @@
+# Python is a case sensitive language...
+
+## Keywords :-
+
+# In programming, a keyword is a word that is reserved by a program because the word has a special meaning. Keywords can be commands or parameters. Every programming language has a set of keywords that cannot be used as variable names...
+
+# Python has 33 keywords
+
+## Identifiers :-
+
+# A python identifier is a name used to identify a variable, function, class, module or other object...
+
+# Rules for setting identifiers :-
+
+# 1) can only start with an alphabet or _(underscore)
+# 2) Followed by 0 or more letter, _ and digits
+# 3) Keywords cannot be used as an identifiers...
