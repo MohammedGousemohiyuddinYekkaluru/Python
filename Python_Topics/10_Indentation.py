1cdf60de226e2abba70553f9464e21d085a5aa64
mohammed gouse <gousemohammed163@gmail.com>
2026-04-18 14:26:47 +0530

    All about Indentation in python

---
+++ Python_Topics/10_Indentation.py
@@ -0,0 +1,13 @@
+# Python does not have brackets() or flower brackets{} to represnt the block of code... 
+
+# instead it follows indentation(tab space) to increase code readability.
+
+name = "xyz"
+
+if name == "xyz":
+    print('line 1')
+    print('line 2')
+    if 9 == 9:
+        print('line 4')
+else:
+    print('line 3')
