cf079a3957b5b3c62fda1a0a9e867cc07052a971
mohammed gouse <gousemohammed163@gmail.com>
2026-04-19 16:34:08 +0530

    All about for loop

--- Python_Topics/11_Loops.py
+++ Python_Topics/11_Loops.py
@@ -15,3 +15,24 @@ i = 1
 while i < 11:
     print(number * i)
     i += 1
+
+## for loop -> it is used when we know how many iterations are needed beforehand...
+
+# range function
+list(range(1, 11)) # [1 - 10] 11 is not included
+
+list(range(5)) # [0, 1, 2, 3, 4]
+
+list(range(1, 11, 3)) # step "3" [1, 4, 7, 10]
+
+for i in range(1, 11):
+    print(i)
+
+for i in "Andhra":
+    print(i)
+
+for i in [1, 2, 3, 4]:
+    print(i)
+
+for i in (1, 2, 3, 4):
+    print(i)
