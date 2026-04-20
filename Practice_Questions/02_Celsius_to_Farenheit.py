93754cb97f20b34c3471ea79dff46036360d2d91
mohammed gouse <gousemohammed163@gmail.com>
2026-04-20 17:17:09 +0530

    Write a program that will convert celsius value to fahrenheit

---
+++ Practice_Questions/02_Celsius_to_Farenheit.py
@@ -0,0 +1,7 @@
+# Write a program that will convert celsius value to fahrenheit
+
+celsius_temp = float(input("Enter temperature in celsius : "))
+
+farenhiet_temp = (celsius_temp * 1.8) + 32
+
+print(farenhiet_temp)
