d001b89d74dbf4af1c65071a7bc037b9f8554150
mohammed gouse <gousemohammed163@gmail.com>
2026-04-20 16:38:33 +0530

    All about Built in functions

---
+++ Python_Topics/15_Built_in_Functions.py
@@ -0,0 +1,55 @@
+# 1) print
+print("Hello World")
+
+# 2) input
+input("Enter your name")
+
+# 3) type
+a = True
+type("a")
+
+# 4) type conversion functions
+int(5.6)
+#float, str, list, tuple
+
+# 5) abs -> absolute function
+abs(4) #4
+abs(-4) #4
+
+# 6) pow -> power
+pow(6, 2) #36
+
+# 7) min/max
+min([2, 6, 1, 9])
+max([2, 6, 1, 9])
+
+min("andhra")
+max("andhra")
+
+# 8) round
+c = 22/7
+round(c, 2) #3.14
+
+# 9) divmod
+divmod(5,2) #(2,1) #integer divison and mod
+
+# 10) bin/oct/hex
+oct(4)
+hex(4)
+bin(4)
+
+# 11) id -> address
+a = 3
+id(a)
+
+# 12) ord -> for ascii code of a string
+ord('a') #65
+
+# 13) len
+len("Andhra")
+
+# 14) sum
+sum([1, 2, 3, 4, 5]) #15
+
+# 15) help
+help('print') # documentation of a particular function
