62cc3fa9e0439c57628abffb3562de0ac691349a
mohammed gouse <gousemohammed163@gmail.com>
2026-04-20 16:24:42 +0530

    All about break, pass and continue

---
+++ Python_Topics/14_Break_Continue&pass.py
@@ -0,0 +1,18 @@
+# Break
+
+for i in range(1, 11):
+    if i == 5:
+        break
+    print(i)
+
+# Continue -> to skip the current iteration
+
+for i in range(1, 11):
+    if i == 5:
+        continue
+    print(i)
+
+# pass -> when you dont have logic or you want to come to the code back but you want to write the remaining code & not get any error then use 'pass'
+
+for i in range(1,11):
+    pass
