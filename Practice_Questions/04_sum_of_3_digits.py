4934553e1499fb3c61c314779bbc73155c6bae66
mohammed gouse <gousemohammed163@gmail.com>
2026-04-21 15:35:27 +0530

    Another way to solve the sum of 3 digit number

--- Practice_Questions/04_sum_of_3_digits.py
+++ Practice_Questions/04_sum_of_3_digits.py
@@ -1,10 +1,23 @@
 # Write a program that will give you the sum of 3 digits
 
-number = input("Enter a three digit number : ")
+number_1 = input("Enter a three digit number : ")
 
 sum = 0
 
-for i in number :
+for i in number_1 :
     sum += int(i)
 
 print(sum)
+
+# Another way
+number = int(input("Enter a three digit number : "))
+
+digit_3 = number % 10   #gets the last digit and stores in digit_3
+number = number // 10   # removes the last digit, just 2 digits remains
+
+digit_2 = number % 10
+digit_1 = number // 10
+
+result = digit_1 + digit_2 + digit_3
+
+print(result)
